55045553a7bb9f195ebf11184d7cc2a86f1482d1
kiatsakul2905 <kiatsakul2905@gmail.com>
2026-03-30 23:04:48 +0700

    Use cookies instead of sessions for user login
    Replaced session variables with cookies for user data.

--- login_neon.php
+++ login_neon.php
@@ -12,9 +12,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     if ($user) {
         if (password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['user_id'];
-            $_SESSION['username'] = $user['username'];
-            $_SESSION['full_name'] = $user['full_name'];
+            setcookie("user_id", $user['user_id'], time() + (86400 * 30), "/");
+            setcookie("username", $user['username'], time() + (86400 * 30), "/");
+            setcookie("full_name", $user['full_name'], time() + (86400 * 30), "/");
             header("Location: index.php");
             exit();
         } else {
